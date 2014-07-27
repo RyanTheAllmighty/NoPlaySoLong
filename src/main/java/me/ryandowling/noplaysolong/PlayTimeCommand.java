9d60376d035e27d93b92cbae459ced6b95dd1a63
RyanTheAllmighty <rldowling03@gmail.com>
2014-07-27 11:37:07 +1000

    Allow commands from the console

--- src/main/java/me/ryandowling/noplaysolong/PlayTimeCommand.java
+++ src/main/java/me/ryandowling/noplaysolong/PlayTimeCommand.java
@@ -12,9 +12,11 @@ import java.util.List;
 import me.ryandowling.noplaysolong.exceptions.UnknownPlayerException;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class PlayTimeCommand implements CommandExecutor {
@@ -25,103 +27,105 @@ public class PlayTimeCommand implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        boolean isPlayer = true;
+        Server server = plugin.getServer();
+        ConsoleCommandSender console = server.getConsoleSender();
+
         if (!(sender instanceof Player)) {
-            return false;
+            isPlayer = false;
         }
 
-        Player player = (Player) sender;
-
         if (args.length == 0) {
-            this.printUsage(player);
+            this.printUsage(sender);
             return false;
         }
 
         if (args[0].equals("start") && args.length == 1) {
-            if (!player.hasPermission("noplaysolong.start")) {
-                player.sendMessage(ChatColor.RED
+            if (!sender.hasPermission("noplaysolong.start")) {
+                sender.sendMessage(ChatColor.RED
                         + "You don't have permission to start the playtime counter!");
                 return false;
             } else {
                 if (plugin.start()) {
                     return true;
                 } else {
-                    player.sendMessage(ChatColor.RED + "Playtime already started!");
+                    sender.sendMessage(ChatColor.RED + "Playtime already started!");
                     return false;
                 }
             }
         } else if (args[0].equals("add") && args.length == 3) {
             if (!plugin.hasStarted()) {
-                player.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
+                sender.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
                 return false;
             }
-            if (!player.hasPermission("noplaysolong.playtime.add")) {
-                player.sendMessage(ChatColor.RED
+            if (!sender.hasPermission("noplaysolong.playtime.add")) {
+                sender.sendMessage(ChatColor.RED
                         + "You don't have permission to add time to a players playtime!");
                 return false;
             } else {
                 try {
                     plugin.addPlayTime(args[1], Integer.parseInt(args[2]));
-                    player.sendMessage(ChatColor.GREEN + "Added " + Integer.parseInt(args[2])
+                    sender.sendMessage(ChatColor.GREEN + "Added " + Integer.parseInt(args[2])
                             + " seconds of playtime to " + args[1]);
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
-                    player.sendMessage(ChatColor.RED + "Invalid number of seconds given!");
+                    sender.sendMessage(ChatColor.RED + "Invalid number of seconds given!");
                     return false;
                 }
                 return true;
             }
         } else if (args[0].equals("remove") && args.length == 3) {
             if (!plugin.hasStarted()) {
-                player.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
+                sender.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
                 return false;
             }
-            if (!player.hasPermission("noplaysolong.playtime.remove")) {
-                player.sendMessage(ChatColor.RED
+            if (!sender.hasPermission("noplaysolong.playtime.remove")) {
+                sender.sendMessage(ChatColor.RED
                         + "You don't have permission to remove time from a players playtime!!");
                 return false;
             } else {
                 try {
                     plugin.removePlayTime(args[1], Integer.parseInt(args[2]));
-                    player.sendMessage(ChatColor.GREEN + "Removed " + Integer.parseInt(args[2])
+                    sender.sendMessage(ChatColor.GREEN + "Removed " + Integer.parseInt(args[2])
                             + " seconds of playtime from " + args[1]);
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
-                    player.sendMessage(ChatColor.RED + "Invalid number of seconds given!");
+                    sender.sendMessage(ChatColor.RED + "Invalid number of seconds given!");
                     return false;
                 } catch (UnknownPlayerException e) {
                     e.printStackTrace();
-                    player.sendMessage(ChatColor.RED + e.getMessage());
+                    sender.sendMessage(ChatColor.RED + e.getMessage());
                     return false;
                 }
                 return true;
             }
         } else if (args[0].equals("check")) {
             if (!plugin.hasStarted()) {
-                player.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
+                sender.sendMessage(ChatColor.RED + "Playtime hasn't started yet!");
                 return false;
             }
-            if (args.length == 1) {
-                if (!player.hasPermission("noplaysolong.playtime.check.self")) {
-                    player.sendMessage(ChatColor.RED
+            if (args.length == 1 && isPlayer) {
+                if (!sender.hasPermission("noplaysolong.playtime.check.self")) {
+                    sender.sendMessage(ChatColor.RED
                             + "You don't have permission to check your playtime!");
                     return false;
                 } else {
-                    player.sendMessage(ChatColor.GREEN
+                    sender.sendMessage(ChatColor.GREEN
                             + "You have played for "
                             + plugin.secondsToDaysHoursSecondsString(plugin
-                                    .getPlayerPlayTime(player.getName()))
+                                    .getPlayerPlayTime(sender.getName()))
                             + " and have "
                             + plugin.secondsToDaysHoursSecondsString(plugin
-                                    .getTimeAllowedInSeconds(player.getName())) + " remaining!");
+                                    .getTimeAllowedInSeconds(sender.getName())) + " remaining!");
                     return true;
                 }
-            } else {
-                if (!player.hasPermission("noplaysolong.playtime.check.others")) {
-                    player.sendMessage(ChatColor.RED
+            } else if (args.length == 2) {
+                if (!sender.hasPermission("noplaysolong.playtime.check.others")) {
+                    sender.sendMessage(ChatColor.RED
                             + "You don't have permission to check other players playtime!");
                     return false;
                 } else {
-                    player.sendMessage(ChatColor.GREEN
+                    sender.sendMessage(ChatColor.GREEN
                             + args[1]
                             + " has played for "
                             + plugin.secondsToDaysHoursSecondsString(plugin
@@ -134,31 +138,31 @@ public class PlayTimeCommand implements CommandExecutor {
             }
         }
 
-        this.printUsage(player);
+        this.printUsage(sender);
 
         return false;
     }
 
-    public void printUsage(Player player) {
+    public void printUsage(CommandSender sender) {
         List<String> usage = new ArrayList<String>();
         usage.add(ChatColor.YELLOW + "/playtime usage:");
-        if (player.hasPermission("noplaysolong.playtime.add")) {
+        if (sender.hasPermission("noplaysolong.playtime.add")) {
             usage.add(ChatColor.AQUA + "/playtime add [user] [time]" + ChatColor.RESET
                     + " - Add time in seconds to the user's playtime.");
         }
-        if (player.hasPermission("noplaysolong.playtime.check.others")) {
+        if (sender.hasPermission("noplaysolong.playtime.check.others")) {
             usage.add(ChatColor.AQUA
                     + "/playtime check [user]"
                     + ChatColor.RESET
                     + " - Check the time played and time left for a given user, or if blank, for yourself.");
-        } else if (player.hasPermission("noplaysolong.playtime.check.self")) {
+        } else if (sender.hasPermission("noplaysolong.playtime.check.self")) {
             usage.add(ChatColor.AQUA + "/playtime check" + ChatColor.RESET
                     + " - Check the time played and time left for yourself.");
         }
-        if (player.hasPermission("noplaysolong.playtime.remove")) {
+        if (sender.hasPermission("noplaysolong.playtime.remove")) {
             usage.add(ChatColor.AQUA + "/playtime remove [user] [time]" + ChatColor.RESET
                     + " - Remove time in seconds from the user's playtime.");
         }
-        player.sendMessage(usage.toArray(new String[usage.size()]));
+        sender.sendMessage(usage.toArray(new String[usage.size()]));
     }
 }
